a1496debe27d0f34f364fc0b4fbb13ace36f6161
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-06-13 13:14:45 +0100

    draw a red window!

--- src/main.c
+++ src/main.c
@@ -1,23 +1,83 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include "SDL_events.h"
+#include "SDL_keycode.h"
+#include "SDL_render.h"
+#include "SDL_video.h"
 #include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
 
 bool initialize_window(void) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error initializing SDL.\n");
-        return false;
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initializing SDL.\n");
+    return false;
+  }
+
+  window = SDL_CreateWindow(
+      NULL,
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      800,
+      600,
+      SDL_WINDOW_BORDERLESS
+  );
+  if(!window) {
+      fprintf(stderr, "Error creaing SDL window.\n");
+      return false;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (!renderer) {
+      fprintf(stderr, "Error creating SDL renderer.\n");
+      return false;
+  }
+  
+  return true;
+}
+
+void setup(void) {
+    // TODO
+}
+
+void process_input() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        case SDL_QUIT:
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+                is_running = false;
+            break;
     }
+}
 
-    // TODO: Create a SDL window
+void update() {
+    // TODO
+}
 
-    // TODO: Create a SDL renderer
+void render() {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
-    return true;
+    SDL_RenderPresent(renderer);
 }
 
 int main(void) {
+  is_running = initialize_window();
 
-    initialize_window();
+  setup();
 
-    return 0;
+  while (is_running) {
+      process_input();
+      update();
+      render();
+  }
+
+  return 0;
 }
