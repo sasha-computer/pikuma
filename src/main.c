9176ecda4b500cd77f5c5afad5e69dc59ec85635
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-06-13 12:41:45 +0100

    progress

--- src/main.c
+++ src/main.c
@@ -1,12 +1,23 @@
-// #include <stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
-void initialize_window(void) {
-    SDL_Init(SDL_INIT_EVERYTHING)
+bool initialize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
+
+    // TODO: Create a SDL window
+
+    // TODO: Create a SDL renderer
+
+    return true;
 }
 
 int main(void) {
 
     initialize_window();
+
     return 0;
 }
