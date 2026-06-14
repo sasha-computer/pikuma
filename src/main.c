b39fee7c9e98a570ea119d50e076035991bdb2ff
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-06-14 10:56:01 +0100

    real fullscreen

--- src/main.c
+++ src/main.c
@@ -25,6 +25,12 @@ bool initialize_window(void) {
     return false;
   }
 
+  SDL_DisplayMode display_mode;
+  SDL_GetCurrentDisplayMode(0, &display_mode);
+
+  window_width = display_mode.w;
+  window_height = display_mode.h;
+
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                        window_width, window_height, SDL_WINDOW_BORDERLESS);
@@ -38,6 +44,8 @@ bool initialize_window(void) {
     fprintf(stderr, "Error creating SDL renderer.\n");
     return false;
   }
+
+  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
   return true;
 }
