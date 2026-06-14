fcf1f4b1352864b323ad91c350a25680790a8e86
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-06-14 11:59:06 +0100

    exercise: draw rectangle

--- src/main.c
+++ src/main.c
@@ -79,13 +79,15 @@ void update() {
   // TODO
 }
 
+// Updates the SDL_Texture with contents of `color_buffer` and
+// renders the texture.
 void render_color_buffer() {
   SDL_UpdateTexture(colour_buffer_texture, NULL, color_buffer,
                     (int)(window_width * sizeof(uint32_t)));
   SDL_RenderCopy(renderer, colour_buffer_texture, NULL, NULL);
 }
 
-void clear_color_buffer(uint32_t color) {
+void set_bg(uint32_t color) {
   for (int y = 0; y < window_height; y++) {
     for (int x = 0; x < window_width; x++) {
       color_buffer[(window_width * y) + x] = color;
@@ -93,32 +95,39 @@ void clear_color_buffer(uint32_t color) {
   }
 }
 
-// TODO:
 // Draw a background grid that fills the entire window.
-// Lines should be rendered at every row/col multiple of 10.
+// Lines should be rendered at every row/col multiple of argument `spacing`.
 void draw_grid(int spacing) {
-    uint32_t color_bg = 0xFF000000;
-    uint32_t color_grid = 0xFFFFFFFF;
+  uint32_t color_bg = 0xFF000000;
+  uint32_t color_grid = 0xFFFFFFFF;
 
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            if ((x % spacing) == 0 || (y % spacing) == 0) {
-                color_buffer[(window_width * y) + x] = color_grid;
-            } else {
-                color_buffer[(window_width * y) + x] = color_bg;
-            }
-        }
+  for (int y = 0; y < window_height; y++) {
+    for (int x = 0; x < window_width; x++) {
+      if ((x % spacing) == 0 || (y % spacing) == 0) {
+        color_buffer[(window_width * y) + x] = color_grid;
+      } else {
+        color_buffer[(window_width * y) + x] = color_bg;
+      }
     }
+  }
+}
+
+void draw_rectangle(int offset_x, int offset_y, int width, int height,
+                    uint32_t color) {
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      color_buffer[(window_width * (y + offset_y)) + (x + offset_x)] = color;
+    }
+  }
 }
 
 void render() {
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
 
-  draw_grid(100);
+  // draw_grid(100);
+  draw_rectangle(200, 200, 800, 600, 0xFFFFFFFF);
   render_color_buffer();
-
-  // clear_color_buffer(0xFFFFFF00);
 
   SDL_RenderPresent(renderer);
 }
